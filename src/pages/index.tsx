c4c72a13c3fd04078959e3c39cbcb49f384d6129
alabo-excel <iamalaboexcel@gmail.com>
2023-06-22 22:20:33 +0100

    minimal animation commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react"
 
-
 export default function Home() {
+
   return (
     <Fragment>
       <title>Beningin</title>
@@ -9,14 +9,15 @@ export default function Home() {
         <div className="w-[40%] mx-auto py-32 text-center">
           <img className="w-[80%] mx-auto" src="/images/logo.png" alt="" />
           <div className="text-white uppercase">
-            <h1>Start game</h1>
-            <h1>Continue</h1>
-            <h1>Settings</h1>
-            <h1>Credits</h1>
-            <h1>Exit Game</h1>
+            <h1 className="zoom">Start game</h1>
+            <h1 className="zoom">Continue</h1>
+            <h1 className="zoom">Settings</h1>
+            <h1 className="zoom">Credits</h1>
+            <h1 className="zoom">Exit Game</h1>
           </div>
         </div>
+
       </main>
-    </Fragment>
+    </Fragment >
   )
 }
